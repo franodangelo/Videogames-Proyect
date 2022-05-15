fbaa9d5fcbc56ca5398764fb120065ea90a3f673
Franco <ffrancodangelo@gmail.com>
2022-05-15 20:44:33 -0300

    Changes in src

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,10 +1,8 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('videogame', {
-    id:{
+    id: {
       type: DataTypes.UUID,
       allowNull: false,
       defaultValue: DataTypes.UUIDV4,
@@ -36,9 +34,5 @@ module.exports = (sequelize) => {
       allowNull: false,
       defaultValue: true
     }
-  },
-  {
-    timestamps: false
-  }
-  );
+  });
 };
