2f9b279ec1fd9af11a34d32e6708b8087d2670f9
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-06-30 20:52:21 -0300

    use constants in actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,3 +1,15 @@
+import {
+    GET_VIDEOGAMES,
+    GET_NAME_VIDEOGAME,
+    GET_VIDEOGAME_DETAIL,
+    GET_GENRES,
+    FILTER_BY_CREATION,
+    FILTER_BY_GENRE,
+    ORDER_BY_NAME,
+    ORDER_BY_RATING,
+    DELETE_VIDEOGAME
+} from './constants';
+
 import axios from 'axios';
 
 export const createVideogame = (payload) => {
@@ -16,7 +28,7 @@ export const getVideogames = () => {
         try {
             let getApi = await axios.get("http://localhost:3001/api/videogames");
             dispatch({
-                type: "GET_VIDEOGAMES",
+                type: GET_VIDEOGAMES,
                 payload: getApi.data
             })
         } catch (error) {
@@ -29,24 +41,24 @@ export const getNameVideogame = (payload) => {
     return async (dispatch) => {
         try {
             let info = await axios.get(`http://localhost:3001/api/videogames?name=${payload}`)
-        dispatch({
-            type: 'GET_NAME_VIDEOGAME',
-            payload: info.data
-        })
+            dispatch({
+                type: GET_NAME_VIDEOGAME,
+                payload: info.data
+            })
         } catch (error) {
-            console.log(error)  
+            console.log(error)
         }
     }
 }
 
 export const getVideogameDetail = (id) => {
-    return async(dispatch) => {
+    return async (dispatch) => {
         try {
             let getApi = await axios.get(`http://localhost:3001/api/videogame/${id}`);
-        dispatch({
-            type: "GET_VIDEOGAME_DETAIL",
-            payload: getApi.data
-        })
+            dispatch({
+                type: GET_VIDEOGAME_DETAIL,
+                payload: getApi.data
+            })
         } catch (error) {
             console.log(error)
         }
@@ -57,10 +69,10 @@ export const getGenres = () => {
     return async (dispatch) => {
         try {
             let genresApi = await axios.get(`http://localhost:3001/api/genres`);
-        dispatch({
-            type: "GET_GENRES",
-            payload: genresApi.data
-        })
+            dispatch({
+                type: GET_GENRES,
+                payload: genresApi.data
+            })
         } catch (error) {
             console.log(error)
         }
@@ -69,28 +81,28 @@ export const getGenres = () => {
 
 export function filterByCreation(payload) {
     return {
-        type: "FILTER_BY_CREATION",
+        type: FILTER_BY_CREATION,
         payload
     }
 };
 
 export function filterByGenre(payload) {
     return {
-        type: "FILTER_BY_GENRE",
+        type: FILTER_BY_GENRE,
         payload
     }
 };
 
 export function orderByName(payload) {
     return {
-        type: "ORDER_BY_NAME",
+        type: ORDER_BY_NAME,
         payload
     }
 };
 
 export function orderByRating(payload) {
     return {
-        type: "ORDER_BY_RATING",
+        type: ORDER_BY_RATING,
         payload
     }
 }
@@ -100,7 +112,7 @@ export const deleteVideogame = (id) => {
         try {
             let deleted = await axios.delete(`http://localhost:3001/api/videogame/${id}`, id);
             dispatch({
-                type: "DELETE_VIDEOGAME",
+                type: DELETE_VIDEOGAME,
                 payload: deleted.data
             })
         } catch (error) {
