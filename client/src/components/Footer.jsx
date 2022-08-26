d8f5713b0de45d5fbf227998b0857d9f4d44e699
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-26 19:29:16 -0300

    created a footer and used it in the home page

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div>
+            <div>
+                <a href="https://www.linkedin.com/in/franco-dangelo">Linkedin</a>
+                <a href="https://github.com/franodangelo">GitHub</a>
+            </div>
+            <h1>Created by Franco D'Angelo - 2022</h1>
+        </div>
+    )
+}
