3cf5dd289ec41eff63f29912e07a480de1970514
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-08-30 16:34:49 -0300

    footer done with repository and personal links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <div>
-            <div>
-                <a href="https://www.linkedin.com/in/franco-dangelo">Linkedin</a>
-                <a href="https://github.com/franodangelo">GitHub</a>
+        <div className='flex h-24 w-full px-8 justify-between items-center bg-palette-600 text-white'>
+            <div className='flex flex-col gap-2 items-center'>
+                <h1 className=''>See code:</h1>
+                <a href="https://github.com/franodangelo/Videogames-Proyect"
+                    target="_blank"
+                    rel="noreferrer">
+                    <FaGithub />
+                </a>
             </div>
-            <h1>Created by Franco D'Angelo - 2022</h1>
+            <div className='flex flex-col gap-4 font-bold items-center'>
+                <h1 className='font-light text-sm'>Created by Franco D'Angelo - 2022</h1>
+                <section className='flex gap-8'>
+                    <a href="https://www.linkedin.com/in/franco-dangelo"
+                        target="_blank"
+                        rel="noreferrer">
+                        <FaLinkedinIn />
+                    </a>
+                    <a href="https://github.com/franodangelo"
+                        target="_blank"
+                        rel="noreferrer">
+                        <FaGithub />
+                    </a>
+                </section>
+            </div>
+
         </div>
     )
 }
