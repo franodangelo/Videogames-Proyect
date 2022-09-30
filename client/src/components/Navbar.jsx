1594ed0149c3cc1cfc0f6a0bdedba8c027558ed1
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-30 17:14:29 -0300

    navbar sticky

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./Searchbar";
 export default function NavBar() {
 
     return (
-        <div className='flex fixed z-10 w-full gap-4 px-8 py-8 flex-wrap md:flex-nowrap md:max-h-[80px] md:items-center justify-center md:justify-between text-white bg-palette-600 shadow-md shadow-palette-900/30'>
+        <div className='flex sticky top-0 z-20 w-full gap-4 px-8 py-8 flex-wrap md:flex-nowrap md:max-h-[80px] md:items-center justify-center md:justify-between text-white bg-palette-600 shadow-md shadow-palette-900/30'>
             <Link to='/home'>
                 <h1 className='uppercase font-bold tracking-widest'>Videogames SPA</h1>
             </Link>
