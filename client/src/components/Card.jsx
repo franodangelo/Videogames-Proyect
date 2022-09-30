359112b8e1ee61c4225f3226c02266231b2761e4
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-09-30 19:01:44 -0300

    cleaned preliminary style and structure

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from "react";
 
-export default function Card({ name, img, released, rating, genres }) {
+export default function Card({ name, img, rating, genres }) {
     return (
-        <main className='flex flex-col h-160 md:h-120 lg:h-80 w-full px-4 py-8 justify-center bg-shades-500/20 shadow-lg shadow-palette-600/50'>
-            <div className='flex justify-end'>
-                <h2 className='font-mono font-bold -tracking-widest'>{rating}</h2>
+        <main className="flex flex-col w-full px-4 py-8 justify-center rounded-lg bg-shades-500 shadow-lg shadow-palette-600/50">
+            <h2 className="mb-2 px-2 py-1 self-end text-xs text-white bg-palette-100 rounded-full">{rating}</h2>
+            <div className="h-full w-full lg:h-60 xl:h-48 overflow-hidden rounded-lg">
+                <img className="h-80 md:h-48 lg:h-72 w-full object-cover" src={img} alt="Videogame cover" />
             </div>
-            <div className='h-full w-full lg:h-60 xl:h-48 overflow-hidden'>
-                <img className='h-80 md:h-48 lg:h-72 w-full object-cover' src={img} alt='Videogame cover' />
-            </div>
-            <div>
-                <h1 className='font-bold uppercase'>{name}</h1>
-                <h3>{genres.join(' - ')}</h3>
-                <h4>Released: {released}</h4>
-            </div>
+            <h1 className="py-2 font-bold text-lg text-white uppercase truncate">{name}</h1>
+            <h3 className="text-sm text-white">{genres.slice(0, 3).join(" - ")}</h3>
+            <button className="mt-4">Show detail</button>
         </main>
     )
-};
+}
