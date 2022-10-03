1e6f82b819a2db6297204d931bb09ed1e33ca3e6
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-02 23:03:48 -0300

    still working in detail

--- client/src/views/VgDetail.jsx
+++ client/src/views/VgDetail.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getVideogameDetail } from '../redux/actions';
-import { Link, useNavigate } from 'react-router-dom';
-import { deleteVideogame } from '../redux/actions';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getVideogameDetail, deleteVideogame } from "../redux/actions";
 
 export default function VideogameDetail() {
-    let videogameDetail = useSelector((state) => state.videogameDetail);
+    let videogameDetail = useSelector(state => state.videogameDetail);
     const dispatch = useDispatch();
     const { id } = useParams();
     let navigate = useNavigate();
@@ -17,30 +15,38 @@ export default function VideogameDetail() {
 
     function handleDelete(id) {
         dispatch(deleteVideogame(id));
-        alert('Your videogame was deleted correctly')
-        navigate('/');
+        alert("Your videogame was deleted correctly")
+        navigate("/");
     }
 
     return (
-        <main>
-            <button className='bg-palette-100 text-sm text-white rounded-tr-lg rounded-br-lg'>
-                <Link to='/'><p>Back home</p></Link>
+        <main className="mx-8 my-4">
+            <button className="m-auto bg-palette-100 text-sm text-white rounded-tr-lg rounded-br-lg">
+                <Link to="/">
+                    <p>Back home</p>
+                </Link>
             </button>
-            <div className='flex h-screen m-8'>
-                <div className='flex flex-col basis-4/12 items-center self-center'>
-                    <h1 className='font-bold text-2xl uppercase'>{videogameDetail.name}</h1>
-                    <h2>{videogameDetail.genres?.join(" - ")}</h2>
-                    <h3>Platforms: {videogameDetail.platforms?.join(", ")} </h3>
-                    <h4>Date of released: {videogameDetail.released}</h4>
-                    {typeof videogameDetail.id !== 'number'
+            <div className="flex h-screen">
+                <div className="flex flex-col basis-4/12 self-center">
+                    <h4 className="text-sm">{videogameDetail.released}</h4>
+                    <div className="flex py-2 gap-6">
+                        <h1 className="font-bold text-5xl uppercase">{videogameDetail.name}</h1>
+                    </div>
+                    <h2 className="py-2 font-thin text-2xl">{videogameDetail.genres?.join(" - ")}</h2>
+                    <h3 className="py-2 text-lg">{videogameDetail.platforms?.join(", ")}</h3>
+                    {typeof videogameDetail.id !== "number"
                         ? <button onClick={() => handleDelete(id)}>Delete videogame</button>
                         : null
                     }
                 </div>
-                <div className='flex flex-col basis-8/12'>
-                    <h4>Rating: {videogameDetail.rating}</h4>
-                    <img className='self-end w-auto h-80 object-contain' src={videogameDetail.img} alt="Videogame cover" />
-                    <p className='text-sm' dangerouslySetInnerHTML={{ __html: videogameDetail.description }}></p>
+                <div className="flex flex-col basis-8/12">
+                    <h4 className="z-40 w-fit -mb-12 mr-4 px-2 py-1 self-end font-bold text-sm text-white rounded-lg bg-palette-100">{videogameDetail.rating}</h4>
+                    <div className="w-full overflow-hidden rounded-lg">
+                        <img className="w-[full] object-cover"
+                            src={videogameDetail.img}
+                            alt={`${videogameDetail.name} thumbnail`} />
+                    </div>
+                    <p className="text-sm" dangerouslySetInnerHTML={{ __html: videogameDetail.description }} />
                 </div>
             </div>
         </main>
