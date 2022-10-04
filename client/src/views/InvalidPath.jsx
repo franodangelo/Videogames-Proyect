cf8c6f98852030edf8e34185f369c2d6394e4d1b
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-04 12:47:41 -0300

    spelling fixes

--- client/src/views/InvalidPath.jsx
+++ client/src/views/InvalidPath.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export default function InvalidPath() {
     return (
         <div >
-            <h1>Oops! This route doesn"t exist :/</h1>
-            <p>Don"t worry, you can go back to the <strong>Home page</strong> by clicking the button below</p>
+            <h1>Oops! This route doesn't exist :/</h1>
+            <p>Don't worry, you can go back to the <strong>Home page</strong> by clicking the button below</p>
             <Link to="/"><button >HOME PAGE</button></Link>
         </div>
     )
