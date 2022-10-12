636ee0e5aeeb37e9dc0274b24653a3b5696531c4
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-12 11:47:36 -0300

    working in form style and handleSubmit

--- client/src/views/Form.jsx
+++ client/src/views/Form.jsx
@@ -97,8 +97,7 @@ export default function Form() {
     useEffect(() => {
         dispatch(getGenres())
         dispatch(getVideogames())
-    }
-        , [dispatch])
+    }, [dispatch])
 
     function handleChange(e) {
         setVideogameLocalState({
@@ -132,7 +131,7 @@ export default function Form() {
     function handleSubmit(e) {
         if (!videogameLocalState.name || !videogameLocalState.description || !videogameLocalState.img || !videogameLocalState.released || !videogameLocalState.rating || videogameLocalState.platforms.length < 1 || videogameLocalState.genres.length < 1) {
             e.preventDefault();
-            alert("Some of the inputs are incomplete")
+            alert("Some of the inputs are incomplete");
         }
         else {
             e.preventDefault();
@@ -154,126 +153,117 @@ export default function Form() {
     function handleDeletePlatforms(e) {
         setVideogameLocalState({
             ...videogameLocalState,
-            platforms: videogameLocalState.platforms.filter(p => p !== e)
+            platforms: videogameLocalState.platforms.filter(platform => platform !== e)
         })
     }
 
     function handleDeleteGenres(e) {
         setVideogameLocalState({
             ...videogameLocalState,
-            genres: videogameLocalState.genres.filter(g => g !== e)
+            genres: videogameLocalState.genres.filter(genre => genre !== e)
         })
     }
 
     return (
-        <main className="flex flex-col w-full h-full items-center px-4 py-8 md:p-16 bg-roses-600">
-            <h1 className="w-fit self-center font-bold text-2xl md:text-3xl text-center">Create your own videogame</h1>
-
-            <form className="flex flex-col h-auto w-auto md:items-center p-4 md:p-8 gap-4 md:gap-8"
-                onSubmit={(e) => handleSubmit(e)}>
-
-                <div className="flex flex-col md:flex-row w-full md:gap-8">
-
-                    <section className="flex flex-col md:h-[400px]">
-
-                        <div className="flex flex-col md:w-96 h-32 gap-2">
+        <main className="flex flex-col w-full h-full items-center px-4 py-8 lg:px-16 lg:py-12 gap-4 bg-slate-900">
+            <h1 className="w-fit self-center font-bold text-2xl lg:text-3xl text-center">Create your own game</h1>
+            <form className="flex flex-col h-auto w-full m-auto lg:w-auto lg:items-center p-4 md:p-8 gap-4 lg:gap-8 rounded-lg bg-slate-800"
+                onSubmit={e => handleSubmit(e)}>
+                <div className="flex flex-col lg:flex-row w-full lg:gap-8">
+                    <section className="flex flex-col lg:h-[400px] gap-4">
+                        <div className="flex flex-col lg:w-96 h-32">
                             <label className="text-lg" htmlFor="name">Name</label>
-                            <input required className="p-2  text-sm placeholder-slate-700 text-roses-600"
-                                name="name" type="text" minlength="3" placeholder="3 characters at least"
+                            <input required className="select w-full mt-2 p-2 text-sm"
+                                name="name" type="text" minlength="3" placeholder="Min. 3 characters"
                                 value={videogameLocalState.name}
                                 onChange={(e) => handleChange(e)}>
                             </input>
-                            <div>{formErrors.name && (<p className="font-bold text-xs text-slate-900">{formErrors.name}</p>)}</div>
+                            <div>
+                                {formErrors.name && (<p className="mt-2 font-bold text-xs text-rose-700">{formErrors.name}</p>)}
+                            </div>
                         </div>
-
-                        <div className="flex flex-col md:w-96 h-40 gap-2">
+                        <div className="flex flex-col lg:w-96 h-40">
                             <label className="text-lg" htmlFor="description">Description</label>
-                            <input required className="px-2 py-8  text-sm placeholder-slate-700 text-roses-600"
-                                name="description" type="text" minlength="20"
+                            <input required className="select w-full mt-2 px-2 py-8 text-sm"
+                                name="description" type="text" minlength="20" placeholder="Min. 20 characters"
                                 value={videogameLocalState.description}
-                                onChange={(e) => handleChange(e)}>
+                                onChange={e => handleChange(e)}>
                             </input>
-                            {formErrors.description && (<p className="font-semibold text-sm text-slate-900">{formErrors.description}</p>)}
+                            {formErrors.description && (<p className="mt-2 font-semibold text-sm text-rose-700">{formErrors.description}</p>)}
                         </div>
-
-                        <div className="flex justify-between gap-4 md:gap-0">
-
-                            <div className="flex flex-col w-full md:w-40 h-32 gap-2">
+                        <div className="flex justify-between gap-4 lg:gap-0">
+                            <div className="flex flex-col w-full lg:w-40 h-32 gap-2">
                                 <label className="text-lg" htmlFor="released">Date of release</label>
-                                <input required className="p-2  text-sm placeholder-slate-700 text-roses-600"
+                                <input required className="select w-full p-2 text-sm"
                                     name="released" type="date"
                                     value={videogameLocalState.released}
-                                    onChange={(e) => handleChange(e)}>
+                                    onChange={e => handleChange(e)}>
                                 </input>
-                                {formErrors.released && (<p className="font-semibold text-sm text-slate-900">{formErrors.released}</p>)}
+                                {formErrors.released && (<p className="font-semibold text-sm text-rose-700">{formErrors.released}</p>)}
                             </div>
-
-                            <div className="flex flex-col w-full md:w-40 h-32 gap-2">
+                            <div className="flex flex-col w-full lg:w-40 h-32 gap-2">
                                 <label className="text-lg" htmlFor="rating">Rating</label>
-                                <input required className="p-2  text-sm placeholder-slate-700 text-roses-600"
+                                <input required className="select w-full p-2 text-sm"
                                     name="rating" type="number" min="0" max="5" placeholder="Rate between 1-5"
                                     value={videogameLocalState.rating}
-                                    onChange={(e) => handleChange(e)}>
+                                    onChange={e => handleChange(e)}>
                                 </input>
-                                {formErrors.rating && (<p className="font-semibold text-sm text-slate-900">{formErrors.rating}</p>)}
+                                {formErrors.rating && (<p className="font-semibold text-sm text-rose-700">{formErrors.rating}</p>)}
                             </div>
-
                         </div>
-
                     </section>
-
-                    <section className="flex flex-col md:h-[400px]">
-
-                        <div className="flex flex-col md:w-96 h-32 gap-2">
+                    <section className="flex flex-col lg:h-[400px] gap-4">
+                        <div className="flex flex-col lg:w-96 h-32 gap-2">
                             <label className="text-lg" htmlFor="img">Image (URL)</label>
-                            <input required className="p-2  text-sm placeholder-slate-700 text-roses-600"
+                            <input required className="select w-full p-2 text-sm"
                                 name="img" type="url"
                                 value={videogameLocalState.img}
                                 onChange={(e) => handleChange(e)}>
                             </input>
                         </div>
-
-                        <div className="flex flex-col md:w-96 h-32 md:h-40 gap-2">
+                        <div className="flex flex-col lg:w-96 h-32 lg:h-40">
                             <label className="text-lg" htmlFor="platforms">Platforms</label>
-                            <select required className="p-2  text-sm placeholder-slate-700 text-roses-600"
+                            <select required className="select w-full mt-2 p-2 text-sm"
                                 name="platforms"
-                                onChange={(e) => handleChangePlatforms(e)}>
+                                onChange={e => handleChangePlatforms(e)}>
                                 <option hidden={true}>Select some platforms</option>
-                                {platforms.map(pl => <option className="text-roses-600" value={pl} >{pl}</option>)}
+                                {platforms.map(platform => <option value={platform}>{platform}</option>)}
                             </select>
                             {formErrors.platforms && (<p>{formErrors.platforms}</p>)}
                             <div className="flex flex-wrap gap-2">
-                                {videogameLocalState.platforms.map(p =>
-                                    <p className="px-2 text-sm bg-slate-200 " type="button" onClick={() => handleDeletePlatforms(p)}>{p}</p>
+                                {videogameLocalState.platforms.map(platform =>
+                                    <p className="mt-2 px-2 py-1 text-xs uppercase text-slate-800 bg-slate-200"
+                                        type="button"
+                                        onClick={() => handleDeletePlatforms(platform)}>{platform}
+                                    </p>
                                 )}
                             </div>
                         </div>
-
-                        <div className="flex flex-col md:w-96 h-32 md:h-40 gap-2">
+                        <div className="flex flex-col lg:w-96 h-32 lg:h-40">
                             <label className="text-lg" htmlFor="genres">Genres</label>
-                            <select required className="p-2  text-sm placeholder-slate-700 text-roses-600"
+                            <select required className="select w-full mt-2 p-2 text-sm"
                                 name="genres"
-                                onChange={(e) => handleChangeGenres(e)}>
+                                onChange={e => handleChangeGenres(e)}>
                                 <option hidden={true}>Select some genres</option>
-                                {genres.map(g => <option className="text-roses-600" value={g.name}>{g.name}</option>)}
+                                {genres.map(genre => <option value={genre.name}>{genre.name}</option>)}
                             </select>
                             <div className="flex flex-wrap gap-2">
-                                {videogameLocalState.genres.map(g =>
-                                    <p className="px-2 text-sm text-center bg-slate-200 " type="button" onClick={() => handleDeleteGenres(g)}>{g}</p>
+                                {videogameLocalState.genres.map(genre =>
+                                    <p className="mt-2 px-2 py-1 text-xs uppercase text-slate-800 bg-slate-200"
+                                        type="button"
+                                        onClick={() => handleDeleteGenres(genre)}>
+                                        {genre}
+                                    </p>
                                 )}
                             </div>
                         </div>
-
                     </section>
-
                 </div>
-
-                <div className="flex w-full justify-center md:justify-end gap-2">
+                <div className="flex w-full justify-center lg:justify-end gap-2">
                     <Link to="/"><button className="secondaryButton">Cancel</button></Link>
-                    <button className="primaryButton">Create videogame</button>
+                    <button className="primaryButton" onClick={e => handleSubmit(e)}>Confirm game</button>
                 </div>
             </form>
-
         </main>
     )
 }
