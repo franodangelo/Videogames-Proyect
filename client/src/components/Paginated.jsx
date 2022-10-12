91118120b1e237a3cfb5bb04913a952f68aa2072
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-12 11:06:00 -0300

    paginated ok

--- client/src/components/Paginated.jsx
+++ client/src/components/Paginated.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 export default function Paginated({ videogamesPerPage, allVideogames, paginated }) {
+    
     const pageNumbers = [];
+    
     for (let i = 1; i <= Math.ceil(allVideogames / videogamesPerPage); i++) {
         pageNumbers.push(i)
     }
+
     return (
-        <nav className="py-8">
-            <div className="flex gap-4">
+        <div className="py-10">
+            <div className="flex gap-6">
                 {pageNumbers.length > 0 && pageNumbers.map(n => (
-                    <button className="page"
-                        key={n}
-                        onClick={() => paginated(n)}>{n}
-                    </button>
+                    <button className="page" key={n} onClick={() => paginated(n)}>{n}</button>
                 ))}
             </div>
-        </nav>
+        </div>
     )
 }
