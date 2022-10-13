c3875a20447beb3eecc7f5d4a4e1d6c5bdc646ea
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-13 12:02:36 -0300

    alternative paths done with styles

--- client/src/views/InvalidSearch.jsx
+++ client/src/views/InvalidSearch.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function InvalidSearch() {
     return (
-        <div>
-            <h1>Your search has no results yet</h1>
-            <p>Try again using another words to look for the videogame you want</p>
+        <div className="flex flex-col col-span-full w-full h-[400px] pt-8 md:pt-12 gap-4 items-center text-center">
+            <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl">There are no games related to your search.</h1>
+            <p className="text-lg md:text-xl lg:text-2xl">Try again using another words to look for the game you want.</p>
         </div>
     )
 }
--- client/src/views/NoGamesCreated.jsx
+++ client/src/views/NoGamesCreated.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function NoGamesCreated() {
     return (
-        <div className="flex flex-col w-full h-auto self-center p-4">
-            <h1>You don't have any videogame made by yourself yet.</h1>
-            <p>Why don't you begin to create some and fill this site with them?</p>
-            <p>Press the button in the navigation bar and start creating!</p>
+        <div className="flex flex-col col-span-full w-full h-[400px] pt-8 md:pt-12 gap-4 items-center text-center">
+            <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl">There are no games created yet.</h1>
+            <p className="text-lg md:text-xl lg:text-2xl">Why don't you begin to create some and fill this site with them?</p>
+            <p className="text-sm md:text-base uppercase">Press the <strong>button</strong> in the navigation bar and start!</p>
         </div>
     )
 }
