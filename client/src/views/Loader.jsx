0eaeee6d8246b2853d4ad895fd7dc4c38a8bc1d4
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-10-13 13:10:57 -0300

    new loader

---
+++ client/src/views/Loader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MutatingDots } from "react-loader-spinner";
+
+export default function Loader() {
+    return (
+        <div className="flex flex-col col-span-full w-full min-h-screen pt-24 md:pt-40 gap-4 items-center bg-slate-900">
+            <MutatingDots
+                height="120"
+                width="120"
+                color="#be123c"
+                secondaryColor='#475569'
+                radius='16'
+                ariaLabel="mutating-dots-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />
+        </div>
+    )
+}
