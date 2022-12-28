1f5b8bd3e02a44acb105ee1746ace0b9b39c83b8
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-28 15:48:44 -0300

    better spacing for searchbar and button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,24 +20,27 @@ export default function NavBar() {
     }
 
     return (
-        <nav className="flex sticky top-0 z-20 w-full h-[80px] px-8 py-4 gap-4 justify-evenly md:justify-between text-white bg-slate-800 shadow-md shadow-slate-900">
-            <Link to="/" className="hidden md:flex items-center">
-                <img src="/client/public/finalLogo.svg" class="h-6 mr-2" alt="Gameon Logo" />
+        <nav className="flex sticky top-0 z-20 w-full h-[80px] px-6 md:px-8 py-4 gap-4 justify-evenly md:justify-between items-center text-white bg-slate-800 shadow-md shadow-slate-900">
+            <Link to="/" className="hidden md:flex gap-2 items-center">
+                <img src="/client/public/finalLogo.svg" class="w-6 h-6" alt="Gameon Logo" />
                 <span className="self-center font-semibold text-xl">Gameon</span>
             </Link>
-            <div className="flex justify-between items-center gap-4">
-                <Link to="/" className="md:hidden items-center">
-                    <img src="/client/public/finalLogo.svg" class="h-6" alt="Gameon Logo" />
-                </Link>
-                <div className="flex relative h-full">
-                    <input className="w-[160px] md:w-full px-4 text-sm text-white placeholder-slate-400 bg-slate-700 border border-rose-700 focus:ring-rose-500 focus:border-rose-500"
+            <div className="flex w-full h-fit justify-end items-center gap-4 md:gap-8">
+                {/* <Link to="/" className="md:hidden items-center">
+                    <img src="/client/public/finalLogo.svg" class="w-2 h-2" alt="Gameon Logo" />
+                </Link> */}
+                <div className="flex w-60 sm:w-[360px] max-w-[400px] h-8 md:h-10">
+                    <input
+                        className="w-full px-4 text-sm text-white placeholder-slate-400 bg-slate-700 border border-rose-700 focus:ring-rose-500 focus:border-rose-500"
                         placeholder="Search game..."
                         type="text"
                         id="input"
                         onChange={(e) => handleInputChange(e)}
                         onSubmit={(e) => handleSearchSubmit(e)}
                     />
-                    <div className="secondaryButton flex w-10 p-0 items-center justify-center cursor-pointer" onClick={(e) => handleSearchSubmit(e)}>
+                    <div
+                        className="secondaryButton flex w-10 p-0 items-center justify-center cursor-pointer"
+                        onClick={(e) => handleSearchSubmit(e)}>
                         <HiSearch />
                     </div>
                 </div>
