4e14cc0b76e8981c50798db317d087b5cf27c0b0
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-30 11:37:25 -0300

    pass port to listening when deploy is ready for the backend

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { DB_PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
+  server.listen(`${ DB_PORT }`, () => {
+    console.log(`%s listening at port ${DB_PORT}`);
   })
 })
